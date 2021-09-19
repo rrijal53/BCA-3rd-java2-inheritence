2499bba2f0bff339685ffe3236832747232f3904
rrijal53 <rijalroshan94@gmail.com>
2021-09-19 07:03:15 +0545

    added new fileed

--- src/JListExample.java
+++ src/JListExample.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class JListExample {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JListExample extends JFrame{
+    JList<String> jlist;
+    JButton button;
+
+   public  JListExample(){
+        String[] mylist = new String[]{"BIM", "BBA", "BCA"};
+
+        jlist = new JList<>(mylist);
+        button = new JButton("CLICK ME");
+        add(jlist);
+        add(button);
+        setSize(300,400);
+        setLayout(new FlowLayout());
+        button.addActionListener(e -> button.setText(String.valueOf(jlist.getSelectedIndex())));
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new JListExample();
+    }
 }
+
+
